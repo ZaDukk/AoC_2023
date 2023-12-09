fa9d379437d70b1f6b13d27d02087d9c07dc43f3
Joshua Kaitharam Thomas <100039717+ZaDukk@users.noreply.github.com>
2023-12-09 06:02:34 +0000

    Add files via upload

---
+++ day_09.py
@@ -0,0 +1,25 @@
+import re
+def extract_integers(input_string, allow_negative=False):
+    pattern = r'-?\d+' if allow_negative else r'\d+'
+    integers = re.findall(pattern, input_string)
+    integers = [int(num) for num in integers]
+
+    return integers
+
+with open("input.txt","r") as f:
+    inp = [extract_integers(line,True) for line in f.readlines()]
+
+def getDifferences(sequence):
+
+
+	if sum(sequence)==0 and [sequence[0]]*len(sequence)==sequence:
+		return 0
+	newSequence = []
+	for i in range (len(sequence)-1):
+		newSequence.append(sequence[i+1]-sequence[i])
+	#first time using recursion for this year!
+	return sequence[-1] + getDifferences(newSequence)
+
+
+print(f"p1: {sum(getDifferences(i) for i in inp)}")
+print(f"p2: {sum(getDifferences(i[::-1]) for i in inp)}")
